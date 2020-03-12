f05d00888b4c56453edc1e4f05205b6967d830f7
Kiran93 <tummalapalli.kirankumar@gmail.com>
2020-03-12 13:07:27 +0100

    error handler interceptor

---
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(
+    req: import('@angular/common/http').HttpRequest<any>,
+    next: import('@angular/common/http').HttpHandler
+  ): import('rxjs').Observable<import('@angular/common/http').HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError(error => {
+        if (error.status === 401) {
+          return throwError(error.statusText);
+        }
+      })
+    );
+  }
+}
