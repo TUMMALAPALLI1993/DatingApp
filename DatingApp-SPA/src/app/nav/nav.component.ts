25326502a9a5a44e50f89c880d90c447f17b17ed
Kiran93 <tummalapalli.kirankumar@gmail.com>
2020-03-13 12:02:26 +0100

    Welcome (User Name) implemented

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -11,7 +11,7 @@ import { error } from 'protractor';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
