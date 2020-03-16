151f2eee73a9c38ce048d19e1743eee2a75df6e6
Kiran93 <tummalapalli.kirankumar@gmail.com>
2020-03-16 16:10:36 +0100

    Added age in Automapper and calculateage extension

--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -9,6 +9,7 @@ namespace DatingApp.API.Dtos
         public int Id { get; set; }
         public string Username { get; set; }
         public string Gender { get; set; }
+        public int Age { get; set; }
         public string KnowAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActivity { get; set; }
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -12,9 +12,11 @@ namespace DatingApp.API.Helpers
             //Create mappings (source, destination) mappings
             //For specific user, where we get the PhotoUrl property map from
             CreateMap<User, UserForListDto>()
-                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url));
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<User, UserForDetailedDto>()
-                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url));
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
