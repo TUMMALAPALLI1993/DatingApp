a2cf65e790a52c488ba42c59d7fe644bf68a2c49
Kiran93 <tummalapalli.kirankumar@gmail.com>
2020-03-16 12:58:25 +0100

    Dummy Data via Json and seeding in Data folder

---
+++ DatingApp.API/Models/Photo.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace DatingApp.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+        
+        //These User and UserId will add a cacade delete to to our migrations in photo table
+        //It means if User gets deleted then photos relating also get deleted
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
