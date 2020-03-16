0a01a7d7a61cc15fe7f05e9d371c4fb9cbbae10f
Kiran93 <tummalapalli.kirankumar@gmail.com>
2020-03-16 16:39:05 +0100

    Implemented User service

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../_models/user';
+import { Observable } from 'rxjs';
+
+const httpOption = {
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
-baseUrl = environment.apiUrl;
 
-constructor() { }
+export class UserService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users', httpOption);
+  }
+
+  getUser(id): Observable<User> {
+    return this.http.get<User>(this.baseUrl + 'user/' + id, httpOption);
+  }
 
 }
