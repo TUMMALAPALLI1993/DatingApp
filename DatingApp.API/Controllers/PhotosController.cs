8e5bf780cde527cedd65b996bf458562d85bdf72
Kiran93 <tummalapalli.kirankumar@gmail.com>
2020-03-25 08:48:43 +0100

    create route for uploaded photos to cloudinary

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -40,6 +40,16 @@ namespace DatingApp.API.Controllers
             _cloudinary = new Cloudinary(acc);
         }
 
+        [HttpGet("{id}", Name = "GetPhoto")]
+        public async Task<IActionResult> GetPhoto(int id)
+        {
+            var photoFromRepo = await _repo.GetPhoto(id);
+
+            var photo = _mapper.Map<PhotoForReturnDto>(photoFromRepo);
+
+            return Ok(photo);
+        }
+
         [HttpPost]
         public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto)
         {
@@ -79,9 +89,10 @@ namespace DatingApp.API.Controllers
 
             if (await _repo.SaveAll())
             {
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
                 //Usuallly we should send CreatedAtRouteResult
                 //But, for testing, we are sending Ok
-                return Ok();
+                return CreatedAtRoute("GetPhoto", new {userId = userId, id = photo.Id}, photoToReturn);
             }
 
             return BadRequest("Couldnot add the photo");
--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -44,5 +44,12 @@ namespace DatingApp.API.Data
             //return true if one or more changes
             return await _context.SaveChangesAsync() > 0;
         }
+
+        public async Task<Photo> GetPhoto(int id)
+        {
+            var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
+
+            return photo;
+        }
     }
 }
